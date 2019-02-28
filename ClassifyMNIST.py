cf71adb45bc7274ffb1cc1f4603fa67ae3ba3e00
Konrad Kollnig <konrad.kollnig@rwth-aachen.de>
2019-02-28 09:41:49 +0100

    MNIST BBB: different params + progress bar for epochs

--- ClassifyMNIST.py
+++ ClassifyMNIST.py
@@ -1,14 +1,15 @@
 from MNIST import *
 import numpy as np
+from tqdm import tqdm, trange
 
 # Multiple epochs
 def multipleEpochAnalyis():
 
     #Hyperparameter declaration
-    BATCH_SIZE = 100
+    BATCH_SIZE = 125
     TEST_BATCH_SIZE = 1000
     CLASSES = 10
-    TRAIN_EPOCHS = 5
+    TRAIN_EPOCHS = 600
     SAMPLES = 2
     TEST_SAMPLES = 10
     PI = 0.5
@@ -34,24 +35,24 @@ def multipleEpochAnalyis():
                 LAYERS = LAYERS,\
                 ACTIVATION_FUNCTIONS = np.array(['relu','relu','softmax']))
 
-    for epoch in range(TRAIN_EPOCHS):
+    for epoch in tqdm(range(TRAIN_EPOCHS)):
         mnist.train()
         errorRate.append(1-mnist.test()) # 1-accuracy
 
     errorRate = np.asarray(errorRate)
     np.savetxt('./Results/BBB_epochs_errorRate.csv',errorRate,delimiter=",")
 
-    plt.plot(range(TRAIN_EPOCHS), errorRate, c='royalblue', label='Bayes BackProp')
-    plt.legend()
-    plt.tight_layout()
-    plt.savefig('./Results/MNIST_EPOCHS.png')
-    torch.save(mnist.net.state_dict(), './Models/BBB_MNIST.pth')
+    #plt.plot(range(TRAIN_EPOCHS), errorRate, c='royalblue', label='Bayes BackProp')
+    #plt.legend()
+    #plt.tight_layout()
+    #plt.savefig('./Results/MNIST_EPOCHS.png')
+    #torch.save(mnist.net.state_dict(), './Models/BBB_MNIST.pth')
 
 # Scalar Mixture vs Gaussian
 def MixtureVsGaussianAnalyis():
 
     #Hyperparameter setting
-    BATCH_SIZE = 100
+    BATCH_SIZE = 125
     TEST_BATCH_SIZE = 1000
     CLASSES = 10
     SAMPLES = 2
@@ -98,7 +99,7 @@ def MixtureVsGaussianAnalyis():
                     LAYERS = layer,\
                     ACTIVATION_FUNCTIONS = np.array(['relu','relu','softmax']))
         
-        for epoch in range(TRAIN_EPOCHS):
+        for epoch in tqdm(range(TRAIN_EPOCHS)):
             mnist.train()
             mnistGaussian.train()
         print("Testing begins!")
@@ -111,10 +112,10 @@ def MixtureVsGaussianAnalyis():
 def HyperparameterAnalysis():
     
     #hyper parameter declaration
-    BATCH_SIZE = 100
+    BATCH_SIZE = 1000
     TEST_BATCH_SIZE = 1000
     CLASSES = 10
-    TRAIN_EPOCHS = 600
+    TRAIN_EPOCHS = 100
     SAMPLES = np.array([1,2,5,10]) #possible values of sample size
     TEST_SAMPLES = 10
     PI = np.array([0.25,0.5,0.75]) #possible values of pi
@@ -122,31 +123,36 @@ def HyperparameterAnalysis():
     SIGMA_2 = np.array([6,7,8]) #possible values of sigma2
     INPUT_SIZE = 28*28
     LAYERS = np.array([400,400])
+    LR = [1e-4, 1e-2, 1e-3, 1e-5]
 
     errorRate = []
     for sample in range(SAMPLES.size):
         for pi in range(PI.size):
             for sigma1 in range(SIGMA_1.size):
                 for sigma2 in range(SIGMA_2.size):
+                    for lr in range(len(LR)):
 
-                    mnist = MNIST(BATCH_SIZE = BATCH_SIZE,\
-                                TEST_BATCH_SIZE = TEST_BATCH_SIZE,\
-                                CLASSES = CLASSES,\
-                                TRAIN_EPOCHS = TRAIN_EPOCHS,\
-                                SAMPLES = SAMPLES[sample],\
-                                TEST_SAMPLES = TEST_SAMPLES,\
-                                hasScalarMixturePrior = True,\
-                                PI = PI[pi],\
-                                SIGMA_1 = torch.FloatTensor([math.exp(-SIGMA_1[sigma1])]),\
-                                SIGMA_2 = torch.FloatTensor([math.exp(-SIGMA_2[sigma2])]),\
-                                INPUT_SIZE = INPUT_SIZE,\
-                                LAYERS = LAYERS,\
-                                ACTIVATION_FUNCTIONS = np.array(['relu','relu','softmax']))
+                        mnist = MNIST(BATCH_SIZE = BATCH_SIZE,\
+                                    TEST_BATCH_SIZE = TEST_BATCH_SIZE,\
+                                    CLASSES = CLASSES,\
+                                    TRAIN_EPOCHS = TRAIN_EPOCHS,\
+                                    SAMPLES = SAMPLES[sample],\
+                                    TEST_SAMPLES = TEST_SAMPLES,\
+                                    hasScalarMixturePrior = True,\
+                                    PI = PI[pi],\
+                                    SIGMA_1 = torch.FloatTensor([math.exp(-SIGMA_1[sigma1])]),\
+                                    SIGMA_2 = torch.FloatTensor([math.exp(-SIGMA_2[sigma2])]),\
+                                    INPUT_SIZE = INPUT_SIZE,\
+                                    LAYERS = LAYERS,\
+                                    ACTIVATION_FUNCTIONS = np.array(['relu','relu','softmax']),\
+                                    LR = LR[lr])
 
-                    for epoch in range(TRAIN_EPOCHS):
-                        mnist.train()
-                    
-                    errorRate.append([SAMPLES[sample],PI[pi],SIGMA_1[sigma1],SIGMA_2[sigma2],1-mnist.test()])
+                        print(SAMPLES[sample],PI[pi],SIGMA_1[sigma1],SIGMA_2[sigma2],LR[lr])
+
+                        for epoch in tqdm(range(TRAIN_EPOCHS)):
+                            mnist.train()
+                        
+                        errorRate.append([SAMPLES[sample],PI[pi],SIGMA_1[sigma1],SIGMA_2[sigma2],LR[lr],1-mnist.test()])
 
     errorRate = np.asarray(errorRate)
     np.savetxt('./Results/BBB_hyperparameters.csv',errorRate,delimiter=",")
