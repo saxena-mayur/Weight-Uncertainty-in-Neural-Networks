8d81f54bf6475cbb6dd0ab77545e028be68f3945
Konrad Kollnig <5175206+kasnder@users.noreply.github.com>
2019-03-06 21:17:27 +0000

    Refurbish SGD

--- run_all_SGD.sh
+++ run_all_SGD.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-Ps=( 1 2 3 4 )
+
+MODE=dropout
+
+Ps=( 2 3 ) # for each P in Ps: lr=1e-$P
 for P in "${Ps[@]}"
 do
 
-Is=( 1 2 3 )
+Is=( 400 800 1200 ) # hidden units
 for I in "${Is[@]}"
 do
 	# nohup python3 SGD.py $P $I $1 > results/log_$P\_$I.txt &
-	CUDA_VISIBLE_DEVICES=0 python3 SGD.py $P $I $1 > results/log_$P\_$I.txt
+	CUDA_VISIBLE_DEVICES=1 python3 SGD.py $P $I $1 $MODE > Results/SGD_MNIST_$MODE\_$I\_1e-$P\.txt
 done
 done
-#watch nvidia-smi
