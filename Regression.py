416f054b407d2acdba023b636cdc06121458d828
“saxena-mayur” <mayur.saxena@seh.ox.ac.uk>
2019-03-23 12:52:53 +0000

    Regression with simple NN and graph improvement

--- Regression.py
+++ Regression.py
@@ -38,58 +38,119 @@ else:
 x = np.random.uniform(-0.1, 0.61, size=(NUM_BATCHES,BATCH_SIZE))
 noise = np.random.normal(0, 0.02, size=(NUM_BATCHES,BATCH_SIZE)) #metric as mentioned in the paper
 y = x + 0.3*np.sin(2*np.pi*(x+noise)) + 0.3*np.sin(4*np.pi*(x+noise)) + noise
-X = Var(x)
-Y = Var(y)
 
 x_test = np.linspace(-0.5, 1,TEST_BATCH_SIZE)
 y_test = x_test + 0.3*np.sin(2*np.pi*x_test) + 0.3*np.sin(4*np.pi*x_test)
-X_test = Var(x_test)
 
-#Training
-print('Training Begins!')
+def BBB_Regression(x,y,x_test,y_test):
 
-#Declare Network
-net = BayesianNetwork(inputSize = 1,\
-                      CLASSES = CLASSES, \
-                      layers=np.array([16,16,16]), \
-                      activations = np.array(['relu','relu','relu','none']), \
-                      SAMPLES = SAMPLES, \
-                      BATCH_SIZE = BATCH_SIZE,\
-                      NUM_BATCHES = NUM_BATCHES,\
-                      hasScalarMixturePrior = True,\
-                      PI = PI,\
-                      SIGMA_1 = SIGMA_1,\
-                      SIGMA_2 = SIGMA_2,\
-                      GOOGLE_INIT= False).to(DEVICE)
+    print('BBB Training Begins!')
 
-#Declare the optimizer
-optimizer = optim.SGD(net.parameters(),lr=1e-3,momentum=0.95)
+    X = Var(x)
+    Y = Var(y)
+    X_test = Var(x_test)
 
-for epoch in range(TRAIN_EPOCHS):
-    if (epoch)%10 == 0:
-        print('Epoch: ', epoch)
-    train(net, optimizer,data=X,target=Y,NUM_BATCHES=NUM_BATCHES)
+    #Declare Network
+    net = BayesianNetwork(inputSize = 1,\
+                        CLASSES = CLASSES, \
+                        layers=np.array([16,16,16]), \
+                        activations = np.array(['relu','relu','relu','none']), \
+                        SAMPLES = SAMPLES, \
+                        BATCH_SIZE = BATCH_SIZE,\
+                        NUM_BATCHES = NUM_BATCHES,\
+                        hasScalarMixturePrior = True,\
+                        PI = PI,\
+                        SIGMA_1 = SIGMA_1,\
+                        SIGMA_2 = SIGMA_2,\
+                        GOOGLE_INIT= False).to(DEVICE)
 
-print('Training Ends!')
+    #Declare the optimizer
+    optimizer = optim.SGD(net.parameters(),lr=1e-3,momentum=0.95)
 
-#Testing
-outputs = torch.zeros(TEST_SAMPLES+1, TEST_BATCH_SIZE, CLASSES).to(DEVICE)
-for i in range(TEST_SAMPLES):
-    outputs[i] = net.forward(X_test)
-outputs[TEST_SAMPLES] = net.forward(X_test)
-pred_mean = outputs.mean(0).data.cpu().numpy().squeeze(1) #Compute mean prediction
-pred_std = outputs.std(0).data.cpu().numpy().squeeze(1) #Compute standard deviation of prediction for each data point
+    for epoch in range(TRAIN_EPOCHS):
+        if (epoch)%10 == 0:
+            print('Epoch: ', epoch)
+        train(net, optimizer,data=X,target=Y,NUM_BATCHES=NUM_BATCHES)
 
-#Visualization
-plt.scatter(x, y,marker='x', c='navy', label='target')
-plt.plot(x_test, pred_mean, c='royalblue', label='Prediction')
-plt.fill_between(x_test, pred_mean - 3 * pred_std, pred_mean + 3 * pred_std,
-                     color='cornflowerblue', alpha=.5, label='+/- 3 std')
-plt.plot(x_test, y_test, c='grey', label='truth')
-plt.legend()
-plt.tight_layout()
-plt.savefig('./Results/Regression.png')
-plt.savefig('./Results/Regression.eps', format='eps', dpi=1000)
+    print('Training Ends!')
 
-#Save the trained model
-torch.save(net.state_dict(), './Models/Regression.pth')
+    #Testing
+    outputs = torch.zeros(TEST_SAMPLES+1, TEST_BATCH_SIZE, CLASSES).to(DEVICE)
+    for i in range(TEST_SAMPLES):
+        outputs[i] = net.forward(X_test)
+    outputs[TEST_SAMPLES] = net.forward(X_test)
+    pred_mean = outputs.mean(0).data.cpu().numpy().squeeze(1) #Compute mean prediction
+    pred_std = outputs.std(0).data.cpu().numpy().squeeze(1) #Compute standard deviation of prediction for each data point
+
+    #Visualization
+    plt.fill_between(x_test, pred_mean - 3 * pred_std, pred_mean + 3 * pred_std,
+                        color='cornflowerblue', alpha=.5, label='+/- 3 std')
+    plt.scatter(x, y,marker='x', c='black', label='target')
+    plt.plot(x_test, pred_mean, c='red', label='Prediction')
+    plt.plot(x_test, y_test, c='grey', label='truth')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig('./Results/Regression/Regression_BBB.png')
+    plt.savefig('./Results/Regression/Regression_BBB.eps', format='eps', dpi=1000)
+    plt.clf()
+
+    #Save the trained model
+    torch.save(net.state_dict(), './Models/Regression.pth')
+
+#Comparing to standard neural network
+def NN_Regression(x,y,x_test,y_test):
+
+    print('SGD Training Begins!')
+
+    x = x.flatten()
+    X = Var(x)
+    X = torch.unsqueeze(X,1)
+    
+    y = y.flatten()
+    Y = Var(y)
+    Y = torch.unsqueeze(Y,1)
+    X_test = Var(x_test)
+    X_test = torch.unsqueeze(X_test,1)
+
+    class Net(torch.nn.Module):
+        def __init__(self, n_feature, n_hidden, n_output):
+            super(Net, self).__init__()
+            self.l1 = torch.nn.Linear(n_feature, n_hidden[0])   # hidden layer 1
+            self.l2 =  torch.nn.Linear(n_hidden[0], n_hidden[1])   # hidden layer 2
+            self.l3 =  torch.nn.Linear(n_hidden[1], n_hidden[2])   # hidden layer 3
+            self.predict = torch.nn.Linear(n_hidden[2], n_output)   # output layer
+
+        def forward(self, x):
+            x = F.relu(self.l1(x))      # activation function for hidden layer 1
+            x = F.relu(self.l2(x))      # activation function for hidden layer 2
+            x = F.relu(self.l3(x))      # activation function for hidden layer 3
+            x = self.predict(x)         # linear output
+            return x
+
+    net = Net(n_feature=1, n_hidden=[16,16,16], n_output=1)
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.2)
+    loss_func = torch.nn.MSELoss()  # this is for regression mean squared loss
+
+    for epoch in range(2000):
+        if (epoch)%10 == 0:
+            print('Epoch: ', epoch)
+        prediction = net(X)     # input x and predict based on x
+        loss = loss_func(prediction, Y)     # must be (1. nn output, 2. target)
+        optimizer.zero_grad()   # clear gradients for next train
+        loss.backward()         # backpropagation, compute gradients
+        optimizer.step()        # apply gradients
+    
+    prediction = net(X_test)
+    
+    #Visualization
+    plt.scatter(x, y,marker='x', c='black', label='target')
+    plt.plot(x_test, prediction.detach().numpy(), c='red', label='Prediction')
+    plt.plot(x_test, y_test, c='grey', label='truth')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig('./Results/Regression/Regression_NN.png')
+    plt.savefig('./Results/Regression/Regression_NN.eps', format='eps', dpi=1000)
+    plt.clf()
+
+BBB_Regression(x,y,x_test,y_test)
+NN_Regression(x,y,x_test,y_test)
